6e68d405f7389caa54b8336d7e33118a36dea196
Sergey Yaskov <yaskovdev@gmail.com>
2022-12-28 02:43:04 +0200

    Create Spring Boot project.

---
+++ src/main/java/com/yaskovdev/geneticprogrammingrobocodeserver/GeneticProgrammingRobocodeServerApplication.java
@@ -0,0 +1,13 @@
+package com.yaskovdev.geneticprogrammingrobocodeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneticProgrammingRobocodeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneticProgrammingRobocodeServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yaskovdev/geneticprogrammingrobocodeserver/GeneticProgrammingRobocodeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yaskovdev.geneticprogrammingrobocodeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneticProgrammingRobocodeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
